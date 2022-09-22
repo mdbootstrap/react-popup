fb7111798971b8f0f6f1de0379b51d10199d05a2
MAKUSref <tomaszmakowski002731@gmail.com>
2022-09-22 10:50:56 +0200

    Add examples

---
+++ src/components/Modal.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import {
+  MDBBtn,
+  MDBModal,
+  MDBModalDialog,
+  MDBModalContent,
+  MDBModalHeader,
+  MDBModalTitle,
+  MDBModalBody,
+  MDBModalFooter,
+  MDBContainer,
+} from "mdb-react-ui-kit";
+
+export default function Modal() {
+  const [basicModal, setBasicModal] = useState(false);
+
+  const toggleShow = () => setBasicModal(!basicModal);
+
+  return (
+    <>
+      <MDBContainer className="text-center mt-5">
+        <MDBBtn onClick={toggleShow}>LAUNCH DEMO MODAL</MDBBtn>
+        <MDBModal show={basicModal} setShow={setBasicModal} tabIndex="-1">
+          <MDBModalDialog>
+            <MDBModalContent>
+              <MDBModalHeader>
+                <MDBModalTitle>Modal title</MDBModalTitle>
+                <MDBBtn
+                  className="btn-close"
+                  color="none"
+                  onClick={toggleShow}
+                ></MDBBtn>
+              </MDBModalHeader>
+              <MDBModalBody>...</MDBModalBody>
+
+              <MDBModalFooter>
+                <MDBBtn color="secondary" onClick={toggleShow}>
+                  Close
+                </MDBBtn>
+                <MDBBtn>Save changes</MDBBtn>
+              </MDBModalFooter>
+            </MDBModalContent>
+          </MDBModalDialog>
+        </MDBModal>
+      </MDBContainer>
+    </>
+  );
+}
